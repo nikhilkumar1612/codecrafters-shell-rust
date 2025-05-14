24513f4604f4c86472cf08736b174e6869d1b51f
nikhil kumar <nikhilkumarpps@gmail.com>
2025-05-15 01:45:50 +0530

    The echo builtin #iz3

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,22 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        match input.trim() {
+        let input_trimmed = input.trim();
+        let parts: Vec<&str> = input_trimmed.split_whitespace().collect();
+
+        match input_trimmed {
             "exit 0" => break,
-            _ => println!("{}: command not found", input.trim())
+            str => {
+                let parts: Vec<&str> = str.split_whitespace().collect();
+                if parts.len() > 1 && parts[0] == "echo" {
+                    for part in &parts[1..] {
+                        print!("{} ", part);
+                    }
+                    println!();
+                } else {
+                    println!("{}: command not found", input_trimmed)
+                }
+            }
         }
     }
 }

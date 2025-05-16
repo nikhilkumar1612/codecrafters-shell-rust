90face56b9a67809213ab86f1d2a2cc4bbe12d0f
nikhil kumar <nikhilkumarpps@gmail.com>
2025-05-16 13:10:52 +0530

    The type builtin: builtins #ez5

--- src/builtins/type_builtin.rs
+++ src/builtins/type_builtin.rs
@@ -4,5 +4,7 @@ pub fn type_builtin_handler(input: String) {
     let parts: Vec<&str> = input.split_whitespace().collect();
     if parts.len() == 2 && COMMANDS.contains(&parts[1]) {
         println!("{} is a shell builtin", parts[1]);
+    } else if parts.len() > 1 {
+        println!("{}: not found", parts[1]);
     }
 }
